6fe1b404d07148efa024813c46f9180da46a0d32
Rodrigo de Oliveira Fernandes <rodrigodeoliveira.ti@gmail.com>
2018-09-14 15:27:29 -0300

    Add Models for the Django app

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Review(models.Model):
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE)
+    rating = models.FloatField()
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    ip_address = models.CharField(max_length=20)
+    submission_date = models.DateField()
