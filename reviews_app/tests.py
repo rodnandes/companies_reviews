64a4a083aea17eaad71088cdf5ba10da84046e69
Rodrigo de Oliveira Fernandes <rodrigodeoliveira.ti@gmail.com>
2018-09-15 00:25:14 -0300

    Add tests to reviews_app

--- reviews_app/tests.py
+++ reviews_app/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
-
+from rest_framework.test import APITestCase
 from reviews_app.models import Company, Review
 from django.contrib.auth.models import User
+from django.urls import reverse
+import json
+from rest_framework.test import APIClient
 
 
 class CompanyModelTest(TestCase):
@@ -47,13 +50,76 @@ class ReviewModelTestCase(TestCase):
             submission_date=review['submission_date']
         )
 
-        self.review = review
+        self.review_data = review
 
     def test_review_objects_creation(self):
         reviews = Review.objects.all()
         self.assertEqual(reviews.count(), 1)
-        self.assertEqual(reviews[0].title, self.review['title'])
+        self.assertEqual(reviews[0].title, self.review_data['title'])
 
     def test_string_representation(self):
         review = Review(title="My review for the company")
         self.assertEqual(str(review), review.title)
+
+
+class ReviewViewSetTestCase(APITestCase):
+
+    def setUp(self):
+
+        self.user = User.objects.create(username="me")
+
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+        self.company = Company.objects.create(name="Some Company")
+        self.review = Review.objects.create(
+            company=self.company,
+            reviewer=self.user,
+            rating=4,
+            title="My review",
+            summary="Just awesome",
+            ip_address="1.1.1.1",
+            submission_date="2018-09-14"
+        )
+
+    def test_post_a_review(self):
+        new_review = {
+            'company': 1,
+            'reviewer': 1,
+            'rating': 3,
+            'title': 'Ok',
+            'summary': 'Is good',
+            'ip_address': '1.1.1.1',
+            'submission_date': '2018-09-14'
+        }
+        response = self.client.post('/api/reviews/', new_review, format='json')
+        self.assertEqual(201, response.status_code)
+
+    def test_list_all_reviews_by_auth_user(self):
+        response = self.client.get('/api/reviews/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(json.loads(response.content)), 1)
+
+    def test_get_one_review(self):
+        response = self.client.get('/api/reviews/1/')
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        self.assertEqual(content['title'], self.review.title)
+
+    def test_edit_review(self):
+        update_data = {
+            'company': 1,
+            'reviewer': 1,
+            'rating': 3,
+            'title': 'updated review',
+            'summary': 'Is good',
+            'ip_address': '1.1.1.1',
+            'submission_date': '2018-09-14'
+        }
+        response = self.client.put('/api/reviews/1/', update_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_review(self):
+
+        response = self.client.delete('/api/reviews/1/')
+        self.assertEqual(response.status_code, 204)
