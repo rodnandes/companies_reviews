2104007df8e3ca44345569733f264851f178889d
Rodrigo de Oliveira Fernandes <rodrigodeoliveira.ti@gmail.com>
2018-09-17 17:12:44 -0300

    Fix Model fields contraints

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Company(models.Model):
@@ -18,11 +19,12 @@ class Review(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     reviewer = models.ForeignKey(settings.AUTH_USER_MODEL,
                                  on_delete=models.CASCADE)
-    rating = models.FloatField()
-    title = models.CharField(max_length=200)
-    summary = models.TextField()
-    ip_address = models.CharField(max_length=20)
-    submission_date = models.DateField()
+    rating = models.FloatField(
+        validators=[MinValueValidator(0.0), MaxValueValidator(0.5)])
+    title = models.CharField(max_length=64)
+    summary = models.TextField(max_length=10000)
+    ip_address = models.GenericIPAddressField()
+    submission_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
