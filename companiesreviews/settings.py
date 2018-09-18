a5b49dd4e64ec9fd521f8311440e098f78d26db9
Rodrigo de Oliveira Fernandes <rodrigodeoliveira.ti@gmail.com>
2018-09-18 02:00:16 -0300

    Change setting to render the API as JSON format

--- companiesreviews/settings.py
+++ companiesreviews/settings.py
@@ -128,5 +128,12 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', )
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
 }
